22ae03c653fdd78ab97237bf427927201fd41dda
Rxssett <62819093+Lxrd17@users.noreply.github.com>
2020-08-22 21:25:50 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     $nombre = $_POST["nombre"];
     $correo = $_POST["correo"];
     $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " . $nombre . "\nCorreo" . $correo . "Mensaje" . $mensaje;
+    $contenido = "GACETIS" . "Nombre: " . $nombre . "\nCorreo" . $correo . "Mensaje" . $mensaje;
     mail($destino, "Gaceta" ,$contenido);
-    header("location:tanks.html")
+    header("location:tanks.html");
 ?>
